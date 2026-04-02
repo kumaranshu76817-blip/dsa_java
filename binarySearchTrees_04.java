16b4239d46bf18c4bac4ab2921237928f60a80c5
Anshu <kumaranshu76817@gmail.com>
2026-04-02 22:02:38 +0530

    Implement binary search tree functionalities including insertion, inorder traversal, search, deletion, and printing paths from root to leaf. Add methods to print values in a specified range. Refactor class names and ensure proper structure across multiple files.

---
+++ binarySearchTrees_04.java
@@ -0,0 +1,73 @@
+//print in range 
+
+import org.w3c.dom.Node;
+
+public class binarySearchTrees_04 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static Node insert(Node root ,int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left = insert(root.left , val);
+        }
+        else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static void printInRange(Node root, int x, int y){
+        if(root == null){
+            return;
+        }
+        if(root.data >= x && root.data <= y){
+            printInRange(root.left, x, y);
+            System.out.print(root.data+" ");
+            printInRange(root.right, x, y);
+        }
+        else if(root.data >= y){
+            printInRange(root.left, x, y);
+        }
+        else{
+            printInRange(root.right, x, y);
+        }
+    }
+    public static void main(String[] args) {
+        int values [] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+
+        }
+
+        inorder(root);
+        System.out.println();
+        
+        printInRange(root, 6, 10);
+        System.out.println();
+
+        printInRange(root, 3, 12);
+        
+
+    }
+}
