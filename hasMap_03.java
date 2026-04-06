0b843a23332a0b5287de7b1c5873cd5fab034494
Anshu <kumaranshu76817@gmail.com>
2026-04-06 21:24:16 +0530

    add hasMap majority element

---
+++ hasMap_03.java
@@ -0,0 +1,30 @@
+
+//majority elemen in java;
+import java.util.*;
+
+public class hasMap_03 {
+    public static void majorityElemetn(int nums []){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = nums.length;
+        for(int i = 0; i<n; i++){
+            if(map.containsKey(nums[i])){ // true
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+
+        for(int key : map.keySet()){
+            if(map.get(key) > n/3){
+                System.out.println(key);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1,5,1,2,1};
+        majorityElemetn(nums);
+    }
+}
